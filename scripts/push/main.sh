efab9dc9ec4e6cdcf78c481d61751096d17541b0
cocoatomo <cocoatomo77@gmail.com>
2020-05-31 17:35:14 +0900

    Create branch 3.9

---
+++ scripts/push/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eux
+
+bash ${TRAVIS_BUILD_DIR}/scripts/${build_type}/prepare-build_3.9
+bash ${TRAVIS_BUILD_DIR}/scripts/${build_type}/upload-catalog
+bash ${TRAVIS_BUILD_DIR}/scripts/${build_type}/renew-catalog-template
