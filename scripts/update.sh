b169cda8bcb4a4e0127d7b74941fd7f784e301df
cocoatomo <cocoatomo77@gmail.com>
2022-04-26 19:34:07 +0900

    Upgrade Sphinx

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,6 +4,9 @@
 [ -n "$GITHUB_ACTIONS" ] && set -x
 set -e
 
+# Update Sphinx
+python -m pip install --upgrade sphinx
+
 # Allow language being passed as 1st argument, defaults to ja
 LANGUAGE=${1:-ja}
 
