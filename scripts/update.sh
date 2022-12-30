ff06042dc11e571f675dbde6684127a41e644f5b
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2022-12-30 05:55:57 -0300

    Improve ci (#40)
    * Use new Transifex CLI tool
    
    The Python implementation named transifex-client uses Transifex's
    APIv2, which is deprecated and will sunset in november 2022.
    
    The new tool, called Transifex CLI, is written in Golang and is
    APIv3-ready. So adjusting the CI and requirements.txt is required.
    
    * Fix condition in the merge job
    
    Having the proper condition is important to have the translation
    update in the latest Python version (python-newest project), and
    the condition wouldn't match the current repository.
    
    Co-authored-by: Maciej Olko <maciej.olko@affirm.com>
    
    * Update sphinx-intl required version
    
    Last released sphinx-intl uses old transifex-client, which uses
    the deprecated Transifex APIv2. There is a fix committed, but
    it is unreleased. So using specific commit where new Transifex
    CLI tool works.
    
    * Update script to work with the new TX command
    
    Previous solution of patching .tx/config does not work with the
    new client, so the script had to be updated with new solution.
    Now, translations are pulled into a sub-directory of cpython, and
    then installed in the languages's repository.
    
    Also using 'realpath' tool in ROOTDIR to retrieve absolute path,
    which actually eases referencing the repository root directory.
    
    * Update actions versions
    
    One of the reason is that some of them use node.js 12, which is
    now deprecated. Actions is displaying warnings about it.
    
    v4 of setup-python is particular useful because it introduces
    package caching, which reduces the time of workflow run.
    
    * Disable pushing translations
    
    Source files auto-update is now done elsewhere
    
    Co-authored-by: Maciej Olko <maciej.olko@affirm.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,9 +7,9 @@ set -e
 # Allow language being passed as 1st argument, defaults to ja
 LANGUAGE=${1:-ja}
 
-ROOTDIR=$(dirname $0)/..
+ROOTDIR=$(realpath "$(dirname $0)/..")
 
-cd ${ROOTDIR}
+cd "${ROOTDIR}"
 
 if ! test -f cpython/Doc/conf.py; then
   echo Unable to find proper CPython Doc folder
@@ -21,22 +21,15 @@ cd cpython/Doc
 sphinx-build -E -b gettext -D gettext_compact=0 -d build/.doctrees . locales/pot
 
 # Update CPython's .tx/config
-cd  locales
+cd locales
 sphinx-intl create-txconfig
-sphinx-intl update-txconfig-resources -p pot -d . --transifex-project-name python-newest
+sphinx-intl update-txconfig-resources -p pot -d . --transifex-organization-name python-doc --transifex-project-name python-newest
 
-if [ "$CI" = true ]
-then
-    tx push --source --no-interactive --skip
-fi
+# Pull translations into cpython/Doc/locales/LANGUAGE/LC_MESSAGES/
+tx pull -l ${LANGUAGE} -t --use-git-timestamps
 
-# Update the translation project's .tx/config
-cd ../../..     # back to $ROOTDIR
-mkdir -p .tx
-sed cpython/Doc/locales/.tx/config \
-  -e '/^source_file/d' \
-  -e 's|<lang>/LC_MESSAGES/||' \
-  -e "s|^file_filter|trans.${LANGUAGE}|" \
-  > .tx/config
-
-tx pull -l ${LANGUAGE} --use-git-timestamps --parallel
+# Finally, move downloaded translation files to the language's repository
+cd "${LANGUAGE}/LC_MESSAGES/"
+for po in $(find . -type f -name '*.po' | sort | sed 's|^\./||'); do
+  install -Dm644 ${po} "${ROOTDIR}/${po}"
+done
